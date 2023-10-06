b09a5e154ff71b140c2f334f860522165904df83
desniland <albertdesmond26@gmail.com>
2023-10-06 10:32:28 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void) {
+int x, y;
+
+for (x = '0'; x <= '9'; x++) {
+for (y = '0'; y <= '9'; y++) {
+if (x != y) {
+putchar(x);
+putchar(y);
+if (x == '8' && y == '9') {
+putchar('\n');
+} else {
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+return (0);
+}
