e19b52abb9408f08cb0ad5d390c2138dabdd306c
desniland <albertdesmond26@gmail.com>
2023-10-06 10:38:54 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,19 +9,17 @@
 int main(void) {
 int x, y;
 
-for (x = '0'; x <= '9'; x++) {
-for (y = '0'; y <= '9'; y++) {
-if (x != y) {
+for (x = '0'; x <= '8'; x++) {
+for (y = x + 1; y <= '9'; y++) {
 putchar(x);
 putchar(y);
-if (x == '8' && y == '9') {
-putchar('\n');
-} else {
+if (x != '8' || y != '9') {
 putchar(',');
 putchar(' ');
 }
 }
 }
+putchar('\n');
+return 0;
 }
-return (0);
-}
+
