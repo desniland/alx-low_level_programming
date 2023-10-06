ebc7dbe916ba8c318af3a090a5196e547755a250
desniland <albertdesmond26@gmail.com>
2023-10-06 08:47:09 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,12 @@
-iinclude <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
