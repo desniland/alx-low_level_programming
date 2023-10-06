57469a62de83eb5db512a9ad186208f761c9fc24
desniland <albertdesmond26@gmail.com>
2023-10-06 11:02:14 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
 */
-int main(void) 
+int main(void)
 {
 int x, y;
 
-for (x = '0'; x <= '8'; x++) 
+for (x = '0'; x <= '8'; x++)
 {
-for (y = x + 1; y <= '9'; y++) 
+for (y = x + 1; y <= '9'; y++)
 {
 putchar(x);
 putchar(y);
-if (x != '8' || y != '9') 
+if (x != '8' || y != '9')
 {
 putchar(',');
 putchar(' ');
@@ -26,4 +29,3 @@ putchar(' ');
 putchar('\n');
 return (0);
 }
-
