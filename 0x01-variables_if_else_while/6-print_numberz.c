dd1371017dd270dd0155e310de9b5f5e01d14db8
desniland <albertdesmond26@gmail.com>
2023-10-06 09:09:56 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,12 +5,16 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
-        int x;
-        for(x='0';x<='9';x++);
-        putchar(x);
-        putchar('\n');
-        return (0);
+char x;
+
+for (x = '0'; x <= '9'; x++)
+{
+putchar(x);
 }
+putchar('\n');
+return (0);
+}
+
