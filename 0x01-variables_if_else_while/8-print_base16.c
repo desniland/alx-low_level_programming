bcc35d7fc22ec29c95488b0b8be03d25f4a5ba41
desniland <albertdesmond26@gmail.com>
2023-10-06 09:32:31 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,15 +5,22 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
-	int x;
-	char y;
-	for(x='0'; x<='9'; x++);
-	putchar(x);
-	for(y='a'; y<='f'; y++);
-	putchar(y);
-	putchar('\n');
-	return (0);
+int x;
+char y;
+
+for (x = '0'; x <= '9'; x++)
+{
+putchar(x);
 }
+for (y = 'a'; y <= 'f'; y++)
+{
+putchar(y);
+}
+putchar('\n');
+return (0);
+}
+
+
