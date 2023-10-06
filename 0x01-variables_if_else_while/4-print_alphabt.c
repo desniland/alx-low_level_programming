1068a88b5909e81b2f9430dc1126597507fd68c0
desniland <albertdesmond26@gmail.com>
2023-10-06 08:27:27 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,15 +5,21 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
-	char x,e,q;
-	e = 'e';
-	q = 'q';
-	for(x='a' ; x<='z' ; x++);
-	if(x!=e && x!=q)
-	putchar(x);
-	putchar('\n');
-	return (0);
+char x, e, q;
+e = 'e';
+q = 'q';
+
+for (x = 'a'; x <= 'z'; x++)
+{
+if (x != e && x != q)
+{
+putchar(x);
 }
+}
+putchar('\n');
+return (0);
+}
+
