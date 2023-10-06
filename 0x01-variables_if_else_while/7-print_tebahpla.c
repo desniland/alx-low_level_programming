55d5ca9cde66463b54ff8b5b6490ded7ff608668
desniland <albertdesmond26@gmail.com>
2023-10-06 04:14:23 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+	for(x='z'; x>='9'; x--);
+	putchar(x);
+	putchar('\n');
+	return (0);
+}
