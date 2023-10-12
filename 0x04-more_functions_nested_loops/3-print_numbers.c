aadb545c367e16e6546ebd150b657821617e52a2
desniland <albertdesmond26@gmail.com>
2023-10-12 04:53:47 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_numbers - print 0123456789
+* 
+*Return: void
+*/
+void print_numbers(void)
+{
+char x;
+
+for (x = '0'; x <= '9'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
