d79389c6644878b43e332cdc6f8abceeb1ccc92a
desniland <albertdesmond26@gmail.com>
2023-10-12 05:22:54 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - printing lines
+ * @x: number of times
+ *
+ * Return: void
+*/
+void print_line(int x)
+{
+int i;
+
+for (i = 0; i < x; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
