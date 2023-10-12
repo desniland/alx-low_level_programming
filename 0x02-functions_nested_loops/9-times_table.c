69d73f057d06f906351ac7c15b525c49cb8c2cf7
desniland <albertdesmond26@gmail.com>
2023-10-12 00:16:44 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - prints out the times table
+ *
+ * Return: No return value (void)
+*/
+void times_table(void)
+{
+int h, v, result;
+
+for (h = 0; h < 10; h++)
+{
+for (v = 0; v < 10; v++)
+{
+result = h * v;
+if (v == 0)
+{
+printf("%2d", result);
+}
+else
+{
+printf(",%2d", result);
+}
+}
+printf("\n");
+}
+}
